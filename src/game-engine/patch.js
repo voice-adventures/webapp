76532bac3ea1a8dfdb46401c5f8ae2f269a06665
Alex Lerman <alexander.a.lerman@gmail.com>
2020-03-08 00:31:16 +0800

    fixing patch

--- src/game-engine/patch.js
+++ src/game-engine/patch.js
@@ -2,7 +2,7 @@ const _ = require('underscore')
 
 module.exports = (game, path, func) => {
   var place = game.gameState
-  for(p of path){
+  for(var p of path){
     if(Array.isArray(place)){
       place = _.find(place, pl => pl.name.toLowerCase() === p.toLowerCase())
     }else {
